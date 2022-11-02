c0ad141b0fe3d0c8065fc646e10fdbac8195dcc8
Mustafanaji0413 <mustafanaji0413@gmail.com>
2022-11-02 23:28:46 +0000

    cacsh the dom and start implementing game functions

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const userScore = 0;
+const botScore = 0;
+const userScoreDiv = document.getElementById("user-point");
+const botScoreDiv = document.getElementById("bot-point");
+const leaderboard = document.getElementsByClassName(".leaderboard");
+const score = document.getElementsByClassName(".score");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissor");
+
+
+rock.addEventListener('click', function () {
+    game("r");
+})
+
+paper.addEventListener('click', function () {
+    game("p");
+})
+
+scissor.addEventListener('click', function () {
+    game("s");
+})
